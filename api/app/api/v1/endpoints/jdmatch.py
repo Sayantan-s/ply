d12787d60b9c24928591ffab16a6de6146e0b15e
Sayantan Samanta <sssamanta789@gmail.com>
2026-01-28 21:47:39 +0530

    feat: jdmatch consumer

--- api/app/api/v1/endpoints/jdmatch.py
+++ api/app/api/v1/endpoints/jdmatch.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, UploadFile, Form, Depends, Request
-from app.modules.jdmatch.service import jd_match
-from typing import Optional, Annotated
+from app.modules.jdmatch.service import jd_match, jd_match_consumer
+from typing import Optional
+from app.core.logging.logger import get_logger
 from app.integrations.upstash.qstash import get_qstash_client, get_qstash_consumer
 from app.modules.jdmatch.schemas import ParseResumeJDInformation
 from qstash.client import QStash
@@ -8,16 +9,22 @@ from qstash.receiver import Receiver
 
 router = APIRouter()
 
+logger = get_logger("jdmatch.api")
+
 @router.post("")
 async def jdmatch(
    file: Optional[UploadFile] = None,
    resume_url: Optional[str] = Form(None),
    qstash: QStash = Depends(get_qstash_client)
 ):
-   return await jd_match(file, resume_url, qstash)
+   logger.info("starting jdmatch()")
+   response = await jd_match(file, resume_url, qstash)
+   logger.info("ending jdmatch()")
+   return response
 
 @router.post("/consumer")
 async def jdmatch_consumer(req: Request, receiver: Receiver = Depends(get_qstash_consumer)):
+   logger.info("starting jdmatch_consumer()")
    _body = await req.body()
    signature, req_body = req.headers["Upstash-Signature"], _body.decode()
    receiver.verify(
@@ -25,4 +32,6 @@ async def jdmatch_consumer(req: Request, receiver: Receiver = Depends(get_qstash
       signature=signature
    )
    payload = ParseResumeJDInformation.model_validate_json(req_body)
+   await jd_match_consumer(payload)
+   logger.info("ending jdmatch_consumer()")
    return {"status": "received", "payload": payload}
--- api/app/modules/jdmatch/service.py
+++ api/app/modules/jdmatch/service.py
@@ -5,11 +5,15 @@ from fastapi import UploadFile, Depends
 from app.modules.jdmatch.repo import save_file, init_file
 from app.integrations.upstash.qstash import get_qstash_client
 from qstash.client import QStash
-
+from app.core.logging.logger import get_logger
+from app.modules.jdmatch.schemas import ParseResumeJDInformation
 
 qstash_dependency = Annotated[QStash, Depends(get_qstash_client)]
+logger = get_logger("jdmatch.service")
+
 
 async def jd_match(file: Optional[UploadFile] = None, resume_url: Optional[str] = None, qstash = qstash_dependency):
+      logger.info("starting jd_match()")
       file_content, filename, file_id = await init_file(file, resume_url)
       
       if not file_id:
@@ -24,8 +28,11 @@ async def jd_match(file: Optional[UploadFile] = None, resume_url: Optional[str] 
             body=resume_info.model_dump(),
       )
 
+      logger.info("ending jd_match()")
       return file_id
 
 
-def consumer():
-      print('Consuming....')
+async def jd_match_consumer(payload: ParseResumeJDInformation):
+      logger.info("starting jd_match_consumer()")
+      logger.info(f'Consuming.... {payload}')
+      logger.info("ending jd_match_consumer()")
